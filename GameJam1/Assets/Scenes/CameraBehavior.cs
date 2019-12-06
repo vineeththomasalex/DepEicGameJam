ca8befef50c41fdb8d861d3c96ba00ff737d71eb
Vineeth <vineeththomasalex@gmail.com>
2019-12-05 16:20:40 -0800

    Added Unity project

---
+++ GameJam1/Assets/Scenes/CameraBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour
+{
+    public Transform Target;
+    public float CameraFollowSpeed = 3.5f;
+    public float yOffset;
+    public float zOffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 followPosition = Target.transform.position;
+        followPosition.y = followPosition.y +yOffset;
+        followPosition.z = followPosition.z +zOffset;
+        this.transform.position = Vector3.Lerp(this.transform.position, followPosition, Time.deltaTime*CameraFollowSpeed);//Linear interpolation
+        this.transform.LookAt(Target, Vector3.up);
+    }
+}
---
+++ GameJam1/Assets/Scenes/MoveTheThang.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTheThang : MonoBehaviour
+{
+    private Vector3 movementDirection;
+    private CharacterController charController;
+    public float MovementSpeed;
+    public float Gravity = -9.8f;
+    // Start is called before the first frame update
+    void Start()
+    {   
+        charController = this.GetComponent<CharacterController>();
+        if(charController == null)
+        {
+            Debug.Log("No Char Controller on this obj:"+ this.name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementDirection = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
+        movementDirection = movementDirection.normalized * MovementSpeed;
+
+        movementDirection.y = Gravity;
+        charController.Move(movementDirection * Time.deltaTime);
+
+    }
+}
